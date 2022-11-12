32140e67e597dd350929161d300981f3ebc73c21
Maria Nijar <r0830560@student.vives.be>
2022-11-12 11:15:20 +0100

    Adding Validatemethods in Model Fish Adding UT for model class Fish

--- Aquarium/Models/Fish.cs
+++ Aquarium/Models/Fish.cs
@@ -6,5 +6,30 @@
         public string? Name { get; set; }
         public string? Species { get; set; }
         public int Length { get; set; }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + " Name: " + Name + " Species: " + Species + " Length: " + Length;
+        }
+        public void ValidateName()
+        {
+            if (Name == null) throw new ArgumentNullException(nameof(Name));
+            if (Name.Length < 2) throw new ArgumentException("Name should be at least 2 char. long");
+        }
+        public void ValidateSpecies()
+        {
+            if (Species == null) throw new ArgumentNullException(nameof(Species));
+            if (Species.Length < 2) throw new ArgumentException(nameof(Species));
+        }
+        public void ValidateLength()
+        {
+            if(Length<0) throw new ArgumentOutOfRangeException(nameof(Length));
+        }
+        public void Validate()
+        {
+            ValidateName();
+            ValidateSpecies();
+            ValidateLength();
+        }
     }
 }
---
+++ AquariumTests/Models/FishTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aquarium.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aquarium.Controllers;
+
+namespace Aquarium.Models.Tests
+{
+    [TestClass()]
+    public class FishTests
+    {
+        private Fish fish = new Fish() { Id=1, Length = 100, Name = "Maria", Species = "Carp" };
+        private Fish nullNameFish = new Fish() { Length = 100, Name = null, Species = "Carp" };
+        private Fish oneCharNameFish = new Fish() { Length = 100, Name = "M", Species = "Carp" };
+        private Fish nullSpeciesFish = new Fish() { Length = 100, Name = "Maria", Species = null };
+        private Fish oneCharSpeciesFish = new Fish() { Length = 100, Name = "Maria", Species = "C" };
+        private Fish negLengthFish = new Fish() { Length = -100, Name = "Maria", Species = "Carp" };
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            fish.ValidateName();
+
+            Assert.ThrowsException<ArgumentNullException>(() => nullNameFish.ValidateName());
+            Assert.ThrowsException<ArgumentException>(() => oneCharNameFish.ValidateName());
+        }
+
+        [TestMethod()]
+        public void ValidateSpeciesTest()
+        {
+            fish.ValidateSpecies();
+
+            Assert.ThrowsException<ArgumentNullException>(() => nullSpeciesFish.ValidateSpecies());
+            Assert.ThrowsException<ArgumentException>(() => oneCharSpeciesFish.ValidateSpecies());
+        }
+
+        [TestMethod()]
+        public void ValidateLengthTest()
+        {
+            fish.ValidateLength();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => negLengthFish.ValidateLength());
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            fish.Validate();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string actual = fish.ToString();
+            string expected = "Id: 1 Name: Maria Species: Carp Length: 100";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+    }
+}
