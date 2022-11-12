ec9ee04c04a36b11e2fd25d7a32bafddf34eb524
Maria Nijar <r0830560@student.vives.be>
2022-11-12 11:55:12 +0100

    Added CORS

--- Aquarium/Program.cs
+++ Aquarium/Program.cs
@@ -5,12 +5,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
